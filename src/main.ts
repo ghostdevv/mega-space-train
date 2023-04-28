847f96e9404198f068bb8b757a66e53b6fe3064f
GHOST <ghostdevbusiness@gmail.com>
2023-04-28 06:26:48 +0000

    feat: train

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { Vector3, SceneLoader, HemisphericLight } from '@babylonjs/core';
 import { createSkybox } from './lib/skybox';
 import { createStars } from './lib/stars';
-import { Vector3 } from '@babylonjs/core';
 import { setup } from './lib/setup';
+import '@babylonjs/loaders';
 import './app.css';
 
 const canvas = document.createElement('canvas')!;
@@ -15,6 +16,8 @@ const { engine, scene } = setup(canvas);
 const { skybox } = createSkybox(scene, resolution);
 const { starMesh } = createStars(scene, starCount);
 
+const _light = new HemisphericLight('light boi', new Vector3(0, 1, 0), scene);
+
 scene.registerBeforeRender(() => {
     starMesh.rotation.addInPlace(new Vector3(0, -0.0003, 0));
     skybox.rotation.addInPlace(new Vector3(0, -0.00006, 0));
@@ -22,3 +25,7 @@ scene.registerBeforeRender(() => {
 
 engine.runRenderLoop(() => scene.render());
 window.addEventListener('resize', () => engine.resize());
+
+SceneLoader.Append('/trains/', 'mega-space-train-one.glb', scene, (scene) => {
+    console.log('hello then');
+});
